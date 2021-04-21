c3b8fc0cf1ec4594efa8a2222dc6da1c34ed72b5
Josh A <jmalm79@gmail.com>
2021-04-21 14:56:38 -0500

    updated path for seed.js

--- seeds/seed.js
+++ seeds/seed.js
@@ -1,4 +1,4 @@
-const sequelize = require('../config/connection');
+const sequelize = require('../config/config');
 const { User, Blogpost } = require('../models');
 
 const userData = require('./userData.json');
