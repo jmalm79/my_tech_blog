1b6370026ad6b9c2c53b563d67df2915633a77c8
Josh A <jmalm79@gmail.com>
2021-04-21 15:06:13 -0500

    added public/ dashboard.js, login.js, logout.js

---
+++ public/js/dashboard.js
@@ -0,0 +1,47 @@
+const newFormHandler = async (event) => {
+    event.preventDefault();
+  
+    const name = document.querySelector('#blogpost-name').value.trim();
+    const description = document.querySelector('#blogpost-desc').value.trim();
+  
+    if (name && description) {
+      const response = await fetch(`/api/blogpost`, {
+        method: 'POST',
+        body: JSON.stringify({ name, description }),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+      });
+  
+      if (response.ok) {
+        document.location.replace('/dashboard');
+      } else {
+        alert('Failed to create blogpost');
+      }
+    }
+  };
+  
+  const delButtonHandler = async (event) => {
+    if (event.target.hasAttribute('data-id')) {
+      const id = event.target.getAttribute('data-id');
+  
+      const response = await fetch(`/api/blogpost/${id}`, {
+        method: 'DELETE',
+      });
+  
+      if (response.ok) {
+        document.location.replace('/dashboard');
+      } else {
+        alert('Failed to delete blogpost');
+      }
+    }
+  };
+  
+  document
+    .querySelector('.new-blogpost-form')
+    .addEventListener('submit', newFormHandler);
+  
+  document
+    .querySelector('.blogpost-list')
+    .addEventListener('click', delButtonHandler);
+  
